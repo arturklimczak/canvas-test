358a073a59853e6f16a04f9595d0ddd5d9a82674
Artur Klimczak <arturklimczak@MacBook-Pro-Artur.local>
2021-01-21 15:15:26 +0100

    canvas editor component

--- canvas/src/app/app.module.ts
+++ canvas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasEditorComponent } from './components/canvas-editor/canvas-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasEditorComponent
   ],
   imports: [
     BrowserModule,
---
+++ canvas/src/app/components/canvas-editor/canvas-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-editor',
+  templateUrl: './canvas-editor.component.html',
+  styleUrls: ['./canvas-editor.component.scss']
+})
+export class CanvasEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
